94ad1150e7a8d12ecfee6ac768ae9123440d7805
Micky Valentino <mickyvalentino18@gmail.com>
2025-03-23 17:35:37 +0700

    feat: fe blasting info

--- frontend/src/pages/BlastingInfo.tsx
+++ frontend/src/pages/BlastingInfo.tsx
@@ -5,22 +5,25 @@ import "../App.css";
 
 const BlastingInfo: React.FC = () => {
   return (
-    <div className="blasting-container">
+    <div className="blasting-info-container">
       {/* Navbar */}
-      <div className="navbar-blasting">
-        <div className="navbar-left-blasting">
-          <Link to="/">
-            <img src={logo} alt="Setetes Harapan" className="logo-blasting" />
-          </Link>
-          <span className="app-title-blasting">SetetesHarapan</span>
-        </div>
-      </div>
-
-      {/* Form Card */}
       <div className="blasting-card">
-        <div className="blasting-content">
-          {/* Left Section */}
-          <div className="blasting-left">
+        <div className="navbar-blasting">
+          <div className="navbar-left-blasting">
+            <Link to="/">
+              <img src={logo} alt="Setetes Harapan" className="logo-blasting" />
+            </Link>
+            <span className="app-title-blasting">SetetesHarapan</span>
+          </div>
+        </div>
+
+        {/* Form Content */}
+        <div className="form-container-blasting">
+          <h2 className="form-title-blasting">Form Blasting Notifikasi</h2>
+
+          {/* Form Grid Layout */}
+          <div className="blasting-content">
+            {/* Left Column */}
             <div className="form-group-blasting">
               <label className="blasting-label">Nama Rumah Sakit</label>
               <select className="blasting-select">
@@ -29,7 +32,9 @@ const BlastingInfo: React.FC = () => {
             </div>
 
             <div className="form-group-blasting">
-              <label className="blasting-label">Kota / Kabupaten Rumah Sakit</label>
+              <label className="blasting-label">
+                Kota / Kabupaten Rumah Sakit
+              </label>
               <select className="blasting-select">
                 <option>Depok</option>
                 <option>Bandung</option>
@@ -46,10 +51,8 @@ const BlastingInfo: React.FC = () => {
                 <option>O</option>
               </select>
             </div>
-          </div>
 
-          {/* Right Section */}
-          <div className="blasting-right">
+            {/* Right Column */}
             <div className="form-group-blasting">
               <label className="blasting-label">Alamat Rumah Sakit</label>
               <select className="blasting-select">
@@ -72,16 +75,21 @@ const BlastingInfo: React.FC = () => {
               </select>
             </div>
           </div>
-        </div>
 
-        {/* Full Width Textarea */}
-        <div className="form-group-blasting full-width">
-          <label className="blasting-label">Tambahan Pesan</label>
-          <textarea className="blasting-textarea" placeholder="Tambahan pesan jika ada"></textarea>
-        </div>
+          {/* Full Width Textarea */}
+          <div className="form-group-blasting full-width">
+            <label className="blasting-label">Tambahan Pesan</label>
+            <textarea
+              className="blasting-textarea"
+              placeholder="Tambahan pesan jika ada"
+            ></textarea>
+          </div>
 
-        {/* Submit Button */}
-        <button className="blasting-button">Sebarkan Notifikasi Sekarang</button>
+          {/* Submit Button */}
+          <button className="submit-button-blasting">
+            Sebarkan Notifikasi Sekarang
+          </button>
+        </div>
       </div>
     </div>
   );
